9e3e4b556985e5a5a77629cd40c9812261ad5cc1
Venice Vargas <venicevargas1@live.com>
2020-09-19 07:41:23 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,9 +1,17 @@
+require 'pry'
+
 class Cat
+  # code goes here
+end 
   attr_accessor :mood
   attr_reader :name
 
   def initialize(name)
     @name = name
     @mood = "nervous"
+  end
+
+  def mood
+    @mood
   end
 end
