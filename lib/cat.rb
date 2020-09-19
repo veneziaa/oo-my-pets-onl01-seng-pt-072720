165d08077bbc04fb6dfd0ee64303adef63f8704e
Venice Vargas <venicevargas1@live.com>
2020-09-19 07:50:57 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,32 +1,17 @@
 class Cat
+  # code goes here
   attr_accessor :mood, :owner
   attr_reader :name
 
-    attr_reader :name
-    attr_accessor :mood, :owner
   @@all = []
 
-    @@all = []
   def initialize(name, owner)
     @name = name
     @mood = "nervous"
     @owner = owner
     @@all << self
-    owner.cats << self
   end
 
-    def initialize(name, owner)
-        @name = name
-        @mood = "nervous"
-        @owner = owner
-        @@all << self
-    end
-
-    def self.all
-        @@all
-    end
-
-end 
   def self.all
     @@all
   end
