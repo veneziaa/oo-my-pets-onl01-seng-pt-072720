bfa8ec59e6bad8acad46ae5597dd3bf226dcd460
Venice Vargas <venicevargas1@live.com>
2020-09-19 07:43:44 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,17 +1,33 @@
-require 'pry'
-
 class Cat
-  # code goes here
-end 
-  attr_accessor :mood
+  attr_accessor :mood, :owner
   attr_reader :name
 
-  def initialize(name)
+    attr_reader :name
+    attr_accessor :mood, :owner
+  @@all = []
+
+    @@all = []
+  def initialize(name, owner)
     @name = name
     @mood = "nervous"
+    @owner = owner
+    @@all << self
+    owner.cats << self
   end
 
-  def mood
-    @mood
+    def initialize(name, owner)
+        @name = name
+        @mood = "nervous"
+        @owner = owner
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+end 
+  def self.all
+    @@all
   end
 end
