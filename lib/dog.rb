ecf6a0b8f4a955553d4ad86bd04341b6e00d8fa1
Venice Vargas <venicevargas1@live.com>
2020-09-19 07:51:05 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
   # code goes here
 end
+class Dog
+  # code goes here
+  attr_accessor :mood, :owner
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name, owner)
+    @name = name
+    @mood = "nervous"
+    @owner = owner
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end 
